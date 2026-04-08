cf4f19c82f7daa6859d03886dd85a64d8b73d814
tuan kiet <tnkiet0512@gmail.com>
2026-04-08 10:42:51 +0700

    (fix): fix role set error in api register

--- backend/src/modules/auth/auth.service.js
+++ backend/src/modules/auth/auth.service.js
@@ -47,7 +47,7 @@ class AuthService {
       email: dto.email,
       password_hash,
       display_name: dto.display_name || dto.displayName,
-      role: 'ROLE_GUARDIAN', // default, will be set after verify
+      role: dto.role, // default, will be set after verify
       email_verified: false,
       is_active: false,
     });
